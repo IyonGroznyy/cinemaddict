f3767a466e2348ff81dd50e3fd0c664ef4c525e5
iyong <iyong@DESKTOP-3LT8NRC>
2021-01-10 23:28:45 +0200

    FirstCommit

---
+++ Cinemaddict/Cinemaddict/Cinemaddict/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Cinemaddict.Services;
+using Cinemaddict.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cinemaddict
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Cinemaddict/Cinemaddict/Cinemaddict/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Cinemaddict.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Cinemaddict.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
