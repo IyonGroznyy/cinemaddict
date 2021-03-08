9c6adc0e7f5f96193a274026d21b7fb0f45ad578
iyong <iyong@DESKTOP-3LT8NRC>
2021-03-09 00:40:18 +0200

    Regex for email and exception has been added/// Error alert has been implemented for Regex check

--- Cinemaddict/Cinemaddict/Cinemaddict/Views/SignUpPage.xaml.cs
+++ Cinemaddict/Cinemaddict/Cinemaddict/Views/SignUpPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -24,6 +25,30 @@ namespace Cinemaddict.Views
 
         private async void SignUpButton_Clicked(object sender, EventArgs e)
         {
+            if (!Regex.IsMatch(EmailEntry.Text.Trim(), @"^([a-zA-Z0-9]+[a-zA-Z0-9\.]*[a-zA-Z0-9]+)@(gmail)\.(com)$", RegexOptions.IgnoreCase))
+            {
+                await DisplayAlert("Authentication Failed", "Email is incorrect", "OK");
+                EmailEntry.Text = "";
+                return;
+            }
+            if (!Regex.IsMatch(PasswordEntry.Text.Trim(), @"[0-9]+", RegexOptions.IgnoreCase))
+            {
+                await DisplayAlert("Authentication Failed", "Password doesn't contain a number", "OK");
+                PasswordEntry.Text = "";
+                return;
+            }
+            if (!Regex.IsMatch(PasswordEntry.Text.Trim(), @"[a-z]+", RegexOptions.IgnoreCase))
+            {
+                await DisplayAlert("Authentication Failed", "Password doesn't contain a letter", "OK");
+                PasswordEntry.Text = "";
+                return;
+            }
+            if (!Regex.IsMatch(PasswordEntry.Text.Trim(), @".{8,}", RegexOptions.IgnoreCase))
+            {
+                await DisplayAlert("Authentication Failed", "Password doesn't contain 8 chars", "OK");
+                PasswordEntry.Text = "";
+                return;
+            }
             await Navigation.PushAsync(new BIOPage(EmailEntry.Text.Trim(), PasswordEntry.Text.Trim()));
         }
 
